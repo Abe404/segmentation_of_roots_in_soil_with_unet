5ba6de2a96a80f8d1904d3c2b72a6136fcbc2900
Sotiris Lamprinidis <sotiris@lamprinidis.com>
2025-01-09 13:56:34 +0100

    Fix mask2former via upscaling

--- src/nn/models/mask2former.py
+++ src/nn/models/mask2former.py
@@ -3,24 +3,18 @@ from transformers import Mask2FormerForUniversalSegmentation, Mask2FormerConfig
 
 models = ["mask2former"]
 
+
 class ModelShim(torch.nn.Module):
-    """Shim for Mask2Former (or other models) to handle the forward pass and crop the output to 388x388."""
-    
     def __init__(self, model):
         super().__init__()
         self.model = model
+        self.upsample = torch.nn.modules.Upsample(
+            scale_factor=4, mode='bilinear')
 
     def forward(self, *args, **kwargs):
-        import ipdb; ipdb.set_trace()
-        # Get the model output
         outputs = self.model(*args, **kwargs)
-
-        # Extract the mask queries logits (segmentation masks)
-        masks = outputs.masks_queries_logits  # (batch_size, num_queries, height, width)
-
-        # Crop the output to 388x388
-        cropped_masks = self.crop_to_388x388(masks)
-
+        masks = outputs.masks_queries_logits
+        cropped_masks = self.crop_to_388x388(self.upsample(masks))
         return cropped_masks
 
     def crop_to_388x388(self, tensor):
@@ -46,19 +40,12 @@ def new(name, encoder_name, pretrained_model, pretrained_backbone):
     pt_name = f"facebook/mask2former-{encoder_name}"
 
     config = Mask2FormerConfig.from_pretrained(pt_name, num_labels=1)
-    # Reduce stride to preserve resolution
-    config.encoder_stride = 1
-    # Reduce patch size to keep the resolution high
-    config.backbone_config.patch_size = 1
-    # Adjust window size to control feature aggregation
-    config.backbone_config.window_size = 2
 
     if pretrained_model:
-        # TODO: doesn't work with the custom resolution-preserving config
         model = Mask2FormerForUniversalSegmentation.from_pretrained(
-            pt_name,
-            config=config
+            pt_name, config=config
         )
     else:
         model = Mask2FormerForUniversalSegmentation(config)
+
     return ModelShim(model)
