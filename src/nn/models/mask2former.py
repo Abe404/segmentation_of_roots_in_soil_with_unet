0c1f076d4ca9d19561a08ea4612655fb61308462
Sotiris Lamprinidis <sotiris@lamprinidis.com>
2025-01-10 16:59:04 +0100

    Fix mask2former

--- src/nn/models/mask2former.py
+++ src/nn/models/mask2former.py
@@ -17,7 +17,7 @@ class ModelShim(torch.nn.Module):
         logits = self.model(*args, **kwargs).masks_queries_logits
         input_shape = args[0].shape[-2:]
         logits = F.interpolate(logits, size=input_shape, mode="bilinear")
-        return crop_to_388x388(logits[:, :1])
+        return crop_to_388x388(logits)
 
 
 def new(name, encoder_name, pretrained_model, pretrained_backbone):
@@ -35,18 +35,17 @@ def new(name, encoder_name, pretrained_model, pretrained_backbone):
     pt_name = f"facebook/mask2former-{encoder_name}"
 
     config = Mask2FormerConfig.from_pretrained(
-        pt_name, num_queries=1, num_labels=1)
+        pt_name, num_queries=1)
 
     if pretrained_model:
         model = Mask2FormerForUniversalSegmentation.from_pretrained(pt_name)
         model.model.transformer_module.queries_embedder = Embedding(
-            model.model.transformer_module.queries_embedder.num_embeddings,
-            model.model.transformer_module.queries_embedder.embedding_dim
+            1, model.model.transformer_module.queries_embedder.embedding_dim
         )
         model.model.transformer_module.queries_features = Embedding(
-            model.model.transformer_module.queries_features.num_embeddings,
-            model.model.transformer_module.queries_features.embedding_dim
+            1, model.model.transformer_module.queries_features.embedding_dim
         )
+        model.num_queries = 1
     else:
         model = Mask2FormerForUniversalSegmentation(config)
     return ModelShim(model)
